9a3ebfffa0921c5c1637782cb88737b47c9b16a5
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-09-28 15:54:36 -0300

    commit 1

--- App.js
+++ App.js
@@ -1,10 +1,82 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+import MapView from "react-native-maps";
+
+/*
+SP: 
+  latitude: -23.5492243,
+  longitude: -46.5813785,
+DF:
+  latitude: -15.8080347,
+  longitude: -47.8750231,
+CG:
+  latitude: -20.4695225,
+  longitude: -54.6016767,
+*/
 
 export default function App() {
+  const [regionData, setRegionData] = useState({
+    latitude: -15.8080347,
+    longitude: -47.8750231,
+    ...delta,
+  });
+
+  const [texto, setTexto] = useState("");
+
+  const delta = {
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View
+        style={{ flexDirection: "row" }}
+      >
+        <Button
+          title="Brasilia"
+          onPress={() => {
+            setRegionData({
+              latitude: -15.8080347,
+              longitude: -47.8750231,
+              ...delta,
+            });
+          }}
+        />
+        <Button
+          title="São Paulo"
+          onPress={() => {
+            setRegionData({
+              latitude: -23.5492243,
+              longitude: -46.5813785,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            });
+          }}
+        />
+      </View>
+      <Text>{regionData.latitude} | {regionData.longitude}</Text>
+      <Text>{texto}</Text>
+      <MapView
+        style={styles.mapa}
+        // initialRegion={{
+        //   latitude: -23.5492243,
+        //   longitude: -46.5813785,
+        //   latitudeDelta: 0.0922,
+        //   longitudeDelta: 0.0421,
+        // }}
+        region={regionData}
+        // onMapReady={() => {Alert.alert("MApa Totalmente Carregado")}} // Ativa quando o mapa for iniciado
+        onRegionChangeComplete={(region) => {
+          setRegionData({
+            latitude: region.latitude,
+            longitude: region.longitude,
+            ...delta,
+          });
+          setTexto(regionData.latitude);
+        }} // Ativa quando o mapa terminar de ser movido
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +89,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  mapa: {
+    width: "100%",
+    height: 550,
+  }
 });
